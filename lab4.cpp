201656dfeadaa9dbe3708b3d2784d054a0112fdc
Izeytee <I-zey@yandex.ru>
2021-02-28 17:16:36 +0700

    lab5

--- lab4.cpp
+++ lab4.cpp
@@ -8,16 +8,18 @@
 #include <cmath>
 #include <cctype>
 #include <set>
+#include <vector>
 
 constexpr int MAX_NUM_AFTER_POINT = 1e6;
 
 struct Node {
-    Node(char c, double p): c(c), p(p), binCode("") { }
+	Node(): c(0), p(0), code("") { }
+    Node(char c, double p): c(c), p(p), code("") { }
     bool operator<(const Node &rhs) const { return this->p < rhs.p; }
 
     char c;
     double p;
-    std::string binCode;
+    std::string code;
 };
 
 const std::string prepareFile(const std::string &fileName)
@@ -90,20 +92,20 @@ std::list<double> calcMean(const std::string &fileName, const size_t symbInRaw)
 	return p;
 }
 
-void printNodes(std::set<Node> &n)
+void printNodes(std::multiset<Node> &n)
 {
     for (auto& it : n)
-        std::cout << it.c << ' ' << it.p << ' ' << it.binCode << '\n';
+        std::cout << it.c << ' ' << it.p << ' ' << it.code << '\n';
 }
 
-std::set<Node> calcNodes(const std::string &fileName)
+std::multiset<Node> calcNodes(const std::string &fileName)
 {
     std::unordered_map<std::string, size_t> strEnc = calcStrEnc(fileName, 1);
 	size_t totalEnc = 0;
 	for (auto &it : strEnc)
 		totalEnc += it.second;
 	
-	std::set<Node> n;
+	std::multiset<Node> n;
 	std::transform(strEnc.cbegin(), strEnc.cend(), std::inserter(n, n.begin()),
 		[&totalEnc](const std::pair<std::string, size_t> &elem) 
 		{ 
@@ -113,26 +115,43 @@ std::set<Node> calcNodes(const std::string &fileName)
 	return n;
 }
 
-void calcHuffmanBinCode(std::set<Node> &n)
+void calcHuffmanCode(std::multiset<Node> &n, size_t base)
 {
     if (n.size() == 1)
         return;
     
-    Node firstN = *n.begin();
-    n.erase(firstN);
-    Node secondN = *n.begin();
-    n.erase(secondN);
-    Node mergedN = Node('#', firstN.p + secondN.p);
+	std::vector<Node> firstFewN(base);
+
+	for (auto curInd = 0; curInd < firstFewN.size(); ++curInd)
+	{
+    	firstFewN[curInd] = *n.begin();
+    	n.erase(n.begin());
+	}
+    Node mergedN = Node('#', std::accumulate(firstFewN.cbegin(), firstFewN.cend(), 0.0, [](double pSum, const Node &N){ return pSum += N.p; }));
     n.insert(mergedN);
 
-    calcHuffmanBinCode(n);
+    calcHuffmanCode(n, base);
 
-    mergedN = *n.find(mergedN);
-    firstN.binCode = mergedN.binCode + '0';
-    secondN.binCode = mergedN.binCode + '1';
-    n.erase(mergedN);
-    n.insert(firstN);
-    n.insert(secondN);
+    auto mergedNIter = n.find(mergedN);
+
+	for (auto curInd = 0; curInd < firstFewN.size(); ++curInd)
+	{
+		firstFewN[curInd].code = mergedNIter->code + char('0' + curInd);
+		n.insert(firstFewN[curInd]);
+	}
+    n.erase(mergedNIter);
+}
+
+void huffmanCode(std::multiset<Node> &n, size_t base)
+{
+	size_t addedAmount = base == 2 ? 0 : base - 2 - (n.size() % (base - 1) + base - 3) % (base - 1);
+	for (size_t iter = 0; iter < addedAmount; ++iter)
+		n.insert(Node('#', 0.0));
+
+	calcHuffmanCode(n, base);
+
+	for (size_t iter = 0; iter < addedAmount; ++iter)
+		n.erase(n.begin());
 }
 
 double calcEntropy(std::list<double> &p, size_t symbInRawAm)
@@ -143,28 +162,56 @@ double calcEntropy(std::list<double> &p, size_t symbInRawAm)
 	return -H / symbInRawAm;
 }
 
-double calcMediumLen(std::set<Node> &n)
+double calcMediumLen(std::multiset<Node> &n)
 {
     double L = 0.0;
     for (auto &it : n)
-        L += it.p * it.binCode.length();
+        L += it.p * it.code.length();
     return L;
+}
+
+void writeEncodedFile(std::multiset<Node> &n, const std::string fileName)
+{
+	std::unordered_map<char, std::string> codeDict;
+	for (auto& it : n)
+		codeDict[it.c] = it.code;
+
+	std::ifstream in(fileName);
+	std::ofstream out("res.txt");
+
+	for(char inputChar; in.get(inputChar);)
+		out << codeDict[inputChar];
 }
 
 void testFile(const std::string fileName)
 {
 	const std::string preparedFile = prepareFile(fileName);
-    std::set<Node> n = calcNodes(preparedFile);
-    calcHuffmanBinCode(n);
+    std::multiset<Node> n = calcNodes(preparedFile);
+
+	size_t base = 3;
+
+    huffmanCode(n, base);
 	printNodes(n);
 
     double L = calcMediumLen(n);
     std::cout << "L: " << L << '\n';
 
-    std::list<double> p = calcMean(preparedFile, 5);
-    double H = calcEntropy(p, 5);
+	if (base == 2)
+	{
+    	std::list<double> p = calcMean(preparedFile, 5);
+    	double H = calcEntropy(p, 5);
+    	std::cout << "r: " << L - H << '\n';
+	}
 
-    std::cout << "r: " << L - H << '\n';
+	writeEncodedFile(n, "test.txt");
+
+	for (size_t symbInRawAm = 1; symbInRawAm <= 8; ++symbInRawAm)
+	{
+		std::list<double> p = calcMean("res.txt", symbInRawAm);
+		std::cout << "For " << symbInRawAm << 
+			" symbols in a raw entropy equals " << calcEntropy(p, symbInRawAm) << '\n';
+	}
+	std::cout << '\n';
 }
 
 int main()
